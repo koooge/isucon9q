0639b3f8c35990ab42e4610dfed1e5400998eda7
ta-koge <ta-koge@global-mobility-service.com>
2019-09-07 13:44:48 +0900

    nginx: Serve public
    Signed-off-by: ta-koge <ta-koge@global-mobility-service.com>

--- deploy-all.sh
+++ deploy-all.sh
@@ -1,5 +1,5 @@
 #/bin/bash -ex
 
 ssh isucon@isucon01 /home/isucon/isucari/webapp/nodejs/deploy.sh
-ssh isucon@isucon02 /home/isucon/isucari/webapp/nodejs/deploy.sh
-ssh isucon@isucon03 /home/isucon/isucari/webapp/nodejs/deploy.sh
+# ssh isucon@isucon02 /home/isucon/isucari/webapp/nodejs/deploy.sh
+# ssh isucon@isucon03 /home/isucon/isucari/webapp/nodejs/deploy.sh
