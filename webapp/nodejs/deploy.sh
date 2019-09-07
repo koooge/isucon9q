8e0c628699ae2f06e56bd9588914a5946855176a
hi-oda <hi-oda@global-mobility-service.com>
2019-09-07 11:26:31 +0900

    add deploy.sh

---
+++ webapp/nodejs/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -ex
+
+cd /home/isucon/isucari/webapp/nodejs
+git pull
+
+npm i
+
+sudo cp ../conf/nginx.conf /etc/nginx
+sudo systemctl restart nginx.service
+
+sudo cp ../conf/my.cnf /etc/mysql
+sudo systemctl restart mysql.service
+
+sudo cp ../conf/isucari.nodejs.service /etc/systemd/system/
+
+sudo systemctl daemon-reload
+sudo systemctl restart isucari.nodejs.service
+
+sudo bash -c 'echo > /var/log/nginx/access.log; echo > /var/log/mysql/slow.log;'
+
+sudo journalctl -f
